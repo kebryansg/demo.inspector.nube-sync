730eb4ef5c98abfbba3d6098c6744e4b57a7d85f
kevinsuarez-recycob <58187988+kevinsuarez-recycob@users.noreply.github.com>
2021-07-21 16:44:01 -0500

    Enabled Cors

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,15 @@ import {HomeControllers} from "./Controllers/HomeControllers";
 
 // creates express app, registers all controller routes and returns you express app instance
 const app = createExpressServer({
-    controllers: [HomeControllers, FileControllers], // we specify controllers we want to use
+    cors: true,
+    controllers: [
+        HomeControllers,
+        FileControllers
+    ], // we specify controllers we want to use
 });
 
 // run express application on port 3000
-app.listen(process.env.PORT ||3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Servidor Iniciado`);
     console.log(`Host: http://localhost:${3000}`);
 });
